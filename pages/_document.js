f383929040f5ddead749085a9c74fc1d01fca05a
Maxim Kucheriaviy <maxim-k@i.ua>
2025-05-23 10:12:28 +0300

    favicon ready

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ import { Header } from "@/components/Header/Header";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Web заліковка</title>
+        <link rel="icon" href="./favicon.ico" sizes="any" />
+      </Head>
       <body>
         <Main />
         <NextScript />
